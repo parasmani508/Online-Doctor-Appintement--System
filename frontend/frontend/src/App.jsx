3ea0911a2adcd0f399d342ab8e175ebc9c45ceb9
Paras kumar <113166283+parasmani508@users.noreply.github.com>
2024-09-03 22:48:39 +0530

    Update App.jsx

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/patient/me",
+          "https://online-doctor-appintement-system-1.onrender.com/api/v1/user/patient/me",
           { withCredentials: true }
         );
         setisAuthenticated(true);
