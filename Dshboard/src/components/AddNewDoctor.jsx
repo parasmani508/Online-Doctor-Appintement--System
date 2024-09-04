fb707d90b1bf2047f99b19227c5b749b827d0238
Paras kumar <113166283+parasmani508@users.noreply.github.com>
2024-09-04 11:52:09 +0530

    Update AddNewDoctor.jsx

--- Dshboard/src/components/AddNewDoctor.jsx
+++ Dshboard/src/components/AddNewDoctor.jsx
@@ -58,7 +58,7 @@ const AddNewDoctor = () => {
       formData.append("doctorDepartment", doctorDepartment);
       formData.append("docAvatar", docAvatar);
      const response= await axios
-        .post("http://localhost:4000/api/v1/user/doctor/addnew", formData, {
+        .post("https://online-doctor-appintement-system-1.onrender.com/api/v1/user/doctor/addnew", formData, {
           withCredentials: true,
           headers: { "Content-Type": "multipart/form-data" },
         })
