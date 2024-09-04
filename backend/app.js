b8341c2205f2b0d6c6be5e8aa2014ab079453e53
Paras kumar <113166283+parasmani508@users.noreply.github.com>
2024-09-04 12:17:10 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ config({path:"./config/config.env"})
 
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL],
+    origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL,'https://online-doctor-appintement-system-2-hplu.onrender.com'],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 }))
